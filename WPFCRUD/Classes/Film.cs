ac2c17945cd70e4517875a33ad2ff7700de32e1f
Vlad <Vlad@DESKTOP-HJ9OVGE>
2019-11-26 20:52:41 +0200

    Initilize project

---
+++ WPFCRUD/Classes/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCRUD.Classes
+{
+    [Serializable]
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+        
+        public float Duration { get; set; }
+
+        public DateTime DateOfCreate { get; set; }
+    }
+}
---
+++ WPFCRUD/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using WPFCRUD.Classes;
+
+namespace WPFCRUD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (!File.Exists("films.xml"))
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(List<Film>));
+                using (FileStream fs = new FileStream("films.xml", FileMode.OpenOrCreate))
+                {
+                    List<Film> films = new List<Film> { };
+                    formatter.Serialize(fs,films);
+                }
+            }
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Film>));
+            using(FileStream fs=new FileStream("films.xml", FileMode.OpenOrCreate))
+            {
+                List<Film> films = (List<Film>)formatter.Deserialize(fs);
+                dgFilms.ItemsSource = films;
+            }
+        }
+    }
+}
